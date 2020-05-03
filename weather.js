39f10281f82aa2f49687cfe87f96fa9d305a51e8
yndvp <yunna3828@gmail.com>
2020-05-03 11:17:45 +0900

    .

--- weather.js
+++ weather.js
@@ -1,4 +1,17 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "4e3b3d9b36805687e8498f7a259a9d8e";
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem('coords', JSON.stringify(coordsObj));
@@ -11,11 +24,12 @@ function handleGeoSuccess(position) {
         latitude,
         longitude
     };
-    saveCoords(coordsObj)
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
-    console.log("Can't aceess geo location");
+    console.log("Can't acess geo location");
 }
 
 function askForCoords() {
@@ -27,7 +41,10 @@ function loadCoords() {
     if(loadedCoords === null) {
         askForCoords();
     } else{
-        //getWeather();
+        const parsedCoords = JSON.parse(loadedCoords);
+        const latitude = parsedCoords.latitude;
+        const longitude = parsedCoords.longitude;
+        getWeather(latitude, longitude);
     }
 }
 
